616c078c736ea955058e18db53ac61d9604be3da
Yaourt404 <remi.loubiou@gmail.com>
2019-12-05 15:45:26 +0100

    eclatement classes

---
+++ src/record/WindowHandler.java
@@ -0,0 +1,18 @@
+package record;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class WindowHandler extends WindowAdapter {
+    HardwareStore h;
+
+    /** ********************************************************
+     *  Method: WindowHandler()
+     ********************************************************/
+    public WindowHandler( HardwareStore s ) { h = s; }
+
+    /** ********************************************************
+     *  Method: windowClosing()
+     ********************************************************/
+    public void windowClosing( WindowEvent e ) { h.cleanup(); }
+ }
---
+++ src/record/main.java
@@ -0,0 +1,10 @@
+package record;
+
+public class main {
+
+	   public static void main( String args[] )
+	   {
+	      HardwareStore hwstore = new HardwareStore();
+	      hwstore.hws = hwstore ;
+	   }
+}
